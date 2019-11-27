91b102cea328638066062cea5e07a18d743c9ec2
Arman Bareyan <abareyan@gmail.com>
2019-11-27 00:28:56 -0700

    Update App.js

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, { Component } from 'react';
+import Card from "./components/Card";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import cars from "./cars.json";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello </h1>
-    </div>
-  );
+let carGuessed = 0;
+let bestScore = 0;
+let head = "Click on the image to guess the car, if you'll click on the same car, you'll lose the game."
+
+
+class App extends Component {
+
+  state = {
+    cars,
+    carGuessed,
+    bestScore,
+    head
+  };
+
 }
+
 
 export default App;
