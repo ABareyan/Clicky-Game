58863f873956ebb1a561fe51121d976d3a630bad
Arman Bareyan <abareyan@gmail.com>
2019-11-27 23:17:58 -0700

    Update App.js

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -19,6 +19,75 @@ class App extends Component {
     head
   };
 
+  setClicked = id => {
+
+    const cars = this.state.cars;
+
+    const clikedCar = cars.filter(car => car.id === id);
+
+    if (clikedCar[0].clicked) {
+
+      console.log("Correct car: " + carGuessed);
+      console.log("Best score: " + bestScore);
+
+      carGuessed = 0;
+      head = "You already clicked on this car"
+
+      for (let i = 0; i < cars.length; i++) {
+        cars[i].clicked = false
+      }
+
+      this.setState({ cars });
+      this.setState({ carGuessed });
+      this.setState({ head });
+
+    } else if (carGuessed < 11) {
+
+      clikedCar[0].clicked = true;
+      carGuessed++;
+      head = "You guessed correctly";
+
+      if (carGuessed > bestScore) {
+        bestScore = carGuessed;
+        this.setState({ bestScore });
+      }
+
+      cars.sort(function(a, b){return 0.5 - Math.random()});
+
+      this.setState({ cars });
+      this.setState({ carGuessed });
+      this.setState({ head });
+    } else {
+
+      clikedCar[0].clicked = true;
+      carGuessed = 0;
+
+      head = "You guessed all cars!";
+      bestScore = 12;
+      this.setState({bestScore});
+
+      for (let i =0; i < cars.length; i++) {
+        clikedCar[i].clicked = false;
+      }
+
+      cars.sort(function(a, b){return 0.5 - Math.random()});
+
+      this.setState({ cars });
+      this.setState({ carGuessed });
+      this.setState({ head });
+    }
+  };
+
+  render() {
+    return (
+      <Main>
+        <Navbar>Clicky Cars Game</Navbar>
+
+    <h3 className="scoreSummary">{this.state.head}</h3>
+      </Main>
+    )
+  }
+
 }
 
 
